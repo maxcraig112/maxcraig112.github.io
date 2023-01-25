5311ff72379865af26e615337a3a890fc48e1621
mcra0009 <mcra0009@student.monash.edu>
2023-01-26 00:50:04 +1100

    Tom's social standing

---
+++ button.js
@@ -0,0 +1,7 @@
+function addrep() {
+    document.getElementById("score").innerHTML = parseInt(document.getElementById("score").innerText) + 1;
+}
+
+function takerep() {
+    document.getElementById("score").innerHTML = parseInt(document.getElementById("score").innerText) - 1000000;
+}
